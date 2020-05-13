f58330bba5ddfc4ed5548bf561947d9dc7800e07
dsteplight <dsteplight@gmail.com>
2020-05-13 18:02:19 -0400

    adding db seeding example

---
+++ app/database/seeds/ApiSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class ApiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('employees')->insert([
+            'title' => Str::random(10),
+            'description' => Str::random(50),
+        ]);
+    }
+}
